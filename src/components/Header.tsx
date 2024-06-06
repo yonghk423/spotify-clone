712a8403c5bc7bcf5c07f3b9f924c7871e281518
yonghee <yonghee@yongheeui-MacBookPro.local>
2024-06-06 14:22:56 +0900

    header ui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const headerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const onScroll = () => {
+            if (!headerRef.current) return;
+            const scrolled = window.scrollY > headerRef.current.offsetHeight;
+            setIsScrolled(scrolled);
+        };
+        window.addEventListener('scroll', onScroll);
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
+
     return (
-        <div className='bg-black flex text-white justify-between'>
+        <div ref={headerRef} className={`h-20 flex text-white justify-between border border-solid border-red-500 fixed w-full ${isScrolled ? 'bg-rgba(0, 0, 255, 0.5)' : 'bg-black'}`}>
             <div>버튼 아이콘</div>
             <div className='flex'>
                 <h1 className="">가입하기</h1>
